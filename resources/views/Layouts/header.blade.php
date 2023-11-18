8953e5715c9005fceb4069616517c16dadfff51e
muhammedabdullah5885 <muhammedabdullah5885@gmail.com>
2023-11-18 13:24:24 +0500

    g
    g

--- resources/views/Layouts/header.blade.php
+++ resources/views/Layouts/header.blade.php
@@ -11,14 +11,59 @@
 
     <link rel="stylesheet" href="{{asset('assets/css/topbar.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    
+  
+  
+    
 </head>
 
 <body>
 
 
-    
+    <style>
+        .float {
+            position: fixed;
+            width: 60px;
+            height: 60px;
+            bottom: 40px;
+            right: 40px;
+            background-color: #25d366;
+            color: #FFF;
+            border-radius: 50px;
+            text-align: center;
+            font-size: 30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index: 100;
+        }
+        .float2 {
+            position: fixed;
+            width: 60px;
+            height: 60px;
+            bottom: 115px;
+            right: 40px;
+            background-color: #007FFF;
+            color: #FFF;
+            border-radius: 50px;
+            text-align: center;
+            font-size: 30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index: 100;
+        }
 
+        .my-float {
+            margin-top: 16px;
+        }
+       
+    </style>
+    <a href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float2" target="_blank">
+        <i class="fa fa-phone my-float"></i>
+    </a>
+
+      <a href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+    </a>
+    
     <!-- header -->
     <div class="w3l-headers-9">
         <header>
@@ -28,7 +73,7 @@
                     <div>
                         <h1>
                             <a href="index.html" class="">
-                                <img src="assets/images/logo-logo.png" style="width: 128px; height:76px; margin-left:-115px" alt="Home Service Logo">
+                                <img src="assets/images/logo-logo.jpg" style="width: 90px;" alt="Home Service Logo">
                             </a>
                         </h1>
                     </div>
